3a6ad714c682161abdc630ed83b1366e4eb446c5
emer033 <165968928+emer033@users.noreply.github.com>
2024-06-05 21:32:09 -0300

    correções pagina login

--- codigo-fonte/js/login.js
+++ codigo-fonte/js/login.js
@@ -1,26 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.getElementById('loginForm');
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("loginForm");
 
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault();
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        const email = document.getElementById('inputemail').value;
-        const senha = document.getElementById('inputsenha').value;
+    const email = document.getElementById("inputemail").value;
+    const senha = document.getElementById("inputsenha").value;
 
-        // Carrega os usuários do localStorage
-        let users = JSON.parse(localStorage.getItem('users')) || [];
+    // Carrega os usuários do localStorage
+    let users = JSON.parse(localStorage.getItem("users")) || [];
 
-        // Verifica se o usuário já existe
-        const userExists = users.some(user => user.email === email && user.senha === senha);
-        if (userExists) {
-            alert('Login feito com sucesso!')
-            window.location.href = '../pages/pagInicial.html';
-        }  else {
-            alert('Email ou senha incorretos!')
-        }
-        
-        // Limpa o formulário
-        loginForm.reset();
+    // Respostas do Login
+    const userExists = users.some(
+      (user) => user.email === email && user.senha === senha && user.email != ""
+    );
+    const incorrectPassword = users.some(
+      (user) => user.email === email && user.senha != senha
+    );
+    if (userExists) {
+      alert("Login feito com sucesso!");
+      window.location.href = "../pages/pagInicial.html";
+    } else if (incorrectPassword) {
+      alert("Senha incorreta!");
+    } else {
+      alert("Este usuário não existe!");
+    }
 
-    });
+    // Limpa o formulário
+    loginForm.reset();
+  });
 });
